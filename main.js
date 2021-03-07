b9e75b4803b8d6cec58ed40cb10ba5509bb75f04
Tirtha Kharel <tkharel21@gmail.com>
2021-03-07 18:48:03 -0500

    non-working test #2

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
-const sum = (a, b) => {
-  return a + b;
-};
+const sum = (a, b) => a + b;
 
-const subtract = (a, b) => {
-  return a - b;
-};
+const subtract = (a, b) => a - b;
 
 export { sum, subtract };
